caa98dfd63b65dbb2b983deccdb1fa6bb0399075
Artem Verenko <artem.verenco@gmail.com>
2024-03-30 13:18:21 +0200

    Boilerplate Chrome Extension

---
+++ src/popup/popup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './popup.css';
+
+const test = <img src='icon.png' alt="icon" />
+
+const rootElement = document.body;
+const root = ReactDOM.createRoot(rootElement);
+
+
+root.render(test);
